f0be390218184b7926d80a98f86b52744f3c085a
loloffs <lukeoloffs@gmail.com>
2021-03-26 16:18:03 +0000

    RESTful API practice walkthrough

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,5 +28,13 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+  RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+
   # [...]
 end
